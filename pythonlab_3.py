b020590efbbd088f65718f82606c84737caedddd
Shruthi-0310 <shruthinat3@gmail.com>
2024-11-28 16:59:21 +0800

    Add files via upload

---
+++ pythonlab_3.py
@@ -0,0 +1,21 @@
+#area of a rectangle
+length=int(input())
+breath=int(input())
+area=length*breath
+print("Area of a rectangle:",area)
+
+#area of a square
+length=int(input())
+area=length*length
+print("Area of a square:",area)
+
+#area of a triangle
+breath=int(input())
+height=int(input())
+area=1/2*breath*height
+print("Area of a triangle:",area)
+
+#area of a circle
+radius=int(input())
+area=3.14*radius*radius
+print("Area of a circle:",area)
